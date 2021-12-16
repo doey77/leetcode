6657b94fcc2d8ee08ee18450cdae96179aeb31a5
Joey David <doey08@gmail.com>
2021-12-16 18:45:02 -0500

    problem setup

--- addTwoNumbers.ts
+++ addTwoNumbers.ts
@@ -6,65 +6,23 @@ class ListNode {
         this.next = (next===undefined ? null : next)
     }
 }
- 
 
-function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-    console.log('hi')
+function addTwoNumbers(l1: ListNode | null,
+    l2: ListNode | null): ListNode | null
+{
+    
 
     return null;
 };
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-/*
-class LinkedNode {
-
-    /**
-     * @param {number} val 
-     * @param {LinkedNode} next 
-     *
-    constructor(val, next) {
-        this.val = val===undefined ? 0 : val;
-        this.next = next===undefined ? null : next;
-    }
-
-    getSize() {
-        let count = 0;
-        let node = this.next;
-        while (node !== undefined) {
-            count++;
-            node = node?.next;
-        }
-        return count;
-    }
-}
-
-/**
- * @param {LinkedNode} l1 
- * @param {LinkedNode} l2 
- *
-const addTwoNumbers = function(l1, l2) {
-
-};
-
 const list1 =
-new LinkedNode(2,
-    new LinkedNode(4,
-        new LinkedNode(3)));
+new ListNode(2,
+    new ListNode(4,
+        new ListNode(3)));
 
 const list2 =
-new LinkedNode(5,
-    new LinkedNode(6,
-        new LinkedNode(4)));
+new ListNode(5,
+    new ListNode(6,
+        new ListNode(4)));
 
-const ex1 = addTwoNumbers(list1, list2); // 342 + 465 = 807 */
+const ex1 = addTwoNumbers(list1, list2); // 342 + 465 = 807
